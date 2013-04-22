9b51d68ca31e37b7861e09316dfc7e6ad7dc07f6
Jason Dreyzehner <Jason@Dreyzehner.com>
2013-04-22 00:27:43 -0400

    init - yo angular mrpigs, y, y, y

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('mrpigsApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
